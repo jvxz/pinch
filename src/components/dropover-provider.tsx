9082e7f214d98ec224f0c4fd9fa7a7d1b30a03f1
jamie 🌨️ <jamie@wisp.bio>
2024-12-21 17:04:05 -0500

    feat: :zap: added commit linting, dropover provider & typography components

---
+++ src/components/dropover-provider.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { AnimatePresence, motion, type Variants } from "framer-motion";
+import { H1, H3, Muted } from "./ui/typography";
+import { ImageUp } from "lucide-react";
+
+const variants: Variants = {
+  initial: { opacity: 0, transition: { duration: 0.1 } },
+  animate: {
+    opacity: 1,
+    transition: { duration: 0.1 },
+  },
+  exit: { opacity: 0, scale: 0.9, transition: { duration: 0.1 } },
+};
+
+export default function DropoverProvider() {
+  const [dragging, setDragging] = useState(false);
+
+  useEffect(() => {
+    // drag enter event
+    function handleEnter(e: DragEvent) {
+      e.preventDefault();
+      setDragging(true);
+    }
+
+    // drag over event
+    function handleOver(e: DragEvent) {
+      e.preventDefault();
+    }
+
+    // drag drop event
+    function handleDrop(e: DragEvent) {
+      e.preventDefault();
+      console.log(e);
+      setDragging(false);
+    }
+
+    document.addEventListener("dragenter", handleEnter);
+    document.addEventListener("dragover", handleOver);
+    document.addEventListener("drop", handleDrop);
+
+    return () => {
+      document.removeEventListener("dragenter", handleEnter);
+      document.removeEventListener("dragover", handleOver);
+      document.removeEventListener("drop", handleDrop);
+    };
+  }, []);
+
+  return (
+    <AnimatePresence>
+      {dragging && (
+        <motion.div
+          className="fixed inset-0 z-50 flex size-full items-center justify-center bg-black/50"
+          variants={variants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+        >
+          <Card className="flex flex-col gap-4 p-8">
+            <ImageUp className="mx-auto size-12" />
+            <H3 className="text-center">Import image</H3>
+            <Muted>Import image by dropping</Muted>
+          </Card>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
